fa80d20e1224c4900cc018bba47bb43103accf00
achrafnejjari <achrafnejjari49@gmail.com>
2025-03-24 22:24:47 +0000

    test create a class in models and verify if its appear in phpmyadmin or no , then display the data of phpmyadmin in a employee.html

--- gestion_ressource_humain/pages/urls.py
+++ gestion_ressource_humain/pages/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),  # http://127.0.0.1:8000/pages/index/
+    path('list_employees/', views.list_employees, name='list_employees'),
 
 ]
--- gestion_ressource_humain/pages/views.py
+++ gestion_ressource_humain/pages/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Employee
 
 def index(request):
     x = {'name': 'ahmed', 'age': 18}
     return render(request, 'html_of_pages/index.html', {'x': x})  # Make sure 'x' is passed as a dictionary
     
+
+def list_employees(request):
+    employees = Employee.objects.all() # Récupérer tous les employés
+    return render(request, 'html_of_pages/employees.html', {'employees': employees}) # Passer les données au template
