ea43d9717a7bf608675a4a882b17e685a6e31706
achrafnejjari <achrafnejjari49@gmail.com>
2025-03-24 21:27:45 +0000

    add dockerfile ,dockercompose(create web , db and phpmyadmin) , dockerignore , requirements and test open the project in browser using docker

--- gestion_ressource_humain/gestion_ressource_humain/settings.py
+++ gestion_ressource_humain/gestion_ressource_humain/settings.py
@@ -77,9 +77,18 @@ WSGI_APPLICATION = 'gestion_ressource_humain.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_database',
+        'USER': 'my_user',
+        'PASSWORD': 'my_password',
+        'HOST': 'db',  # Correspond au service "db" de docker-compose
+        'PORT': '3306',
     }
 }
 
