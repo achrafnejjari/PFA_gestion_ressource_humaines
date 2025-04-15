63b350610b0600c9e0e4acd3e2ad593d5336379d
achrafnejjari <achrafnejjari49@gmail.com>
2025-04-15 20:11:17 +0000

    jai creer pages signup , services , contacts , apropos (html et css ) , et je modifier la page navbar

--- gestion_ressource_humain/pages/views.py
+++ gestion_ressource_humain/pages/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'html_of_pages/index.html') 
+    return render(request, 'html_of_pages/index.html', {'active_page': 'index'}) 
 
 def login(request):
-    return render(request, 'html_of_pages/login.html')
+    return render(request, 'html_of_pages/login.html', {'active_page': 'login'})
 
 def about(request):
-    return render(request, 'html_of_pages/about.html')
+    return render(request, 'html_of_pages/about.html', {'active_page': 'about'})
+
+def navbar(request):
+    return render(request, 'html_of_pages/navbar.html', {'active_page': 'navbar'})
+
+
+def signup(request):
+    return render(request, 'html_of_pages/signup.html', {'active_page': 'signup'})
+
+
+
+def contact(request):
+    return render(request, 'html_of_pages/contact.html', {'active_page': 'contact'})
+
+
+
+def services(request):
+    return render(request, 'html_of_pages/services.html', {'active_page': 'services'})
+
+
+def about(request):
+    return render(request, 'html_of_pages/about.html', {'active_page': 'about'})
