a301dffca6ee5305f6df88b98d1d626e80004dce
tarek <bekkaoui.tarek2001@gmail.com>
2025-05-04 14:48:03 +0100

    Ajouter des modifications

--- gestion_ressource_humain/gestion_ressource_humain/settings.py
+++ gestion_ressource_humain/gestion_ressource_humain/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -143,6 +142,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 # Configuration de l'email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -151,3 +151,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'bk.tarek.48@gmail.com' 
 EMAIL_HOST_PASSWORD = 'extf ybxe vhtl dkvy'  
 DEFAULT_FROM_EMAIL = 'bk.tarek.48@gmail.com'  
+
